f5a1ebb94ec38d40fa5ed4a7b8980101676edf15
rayankhan01 <rk12172002@gmail.com>
2024-03-09 20:13:31 +0500

    Greetings

---
+++ main.js
@@ -0,0 +1,5 @@
+var names = ["Salman", "Maqsoom", "Nubair"];
+for (var _i = 0, names_1 = names; _i < names_1.length; _i++) {
+    var name_1 = names_1[_i];
+    console.log("Hello ".concat(name_1, ",everyone is so busy that no one even comes to meet me."));
+}
---
+++ main.ts
@@ -0,0 +1,3 @@
+let names: string[] = ["Salman","Maqsoom","Nubair"];
+for (let name of names) {console.log(`Hello ${name},everyone is so busy that no one even comes to meet me.`)
+}
